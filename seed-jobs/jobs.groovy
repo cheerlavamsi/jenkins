207d095ef427a7e37602f30026d40717e0df6e44
prasad balannagaru <chvamsi.devops@gmail.com>
2019-12-25 17:57:05 +0530

    add

--- seed-jobs/jobs.groovy
+++ seed-jobs/jobs.groovy
@@ -21,3 +21,28 @@ pipelineJob('studentapp-project/TEST-Pipeline') {
     }
   }
 }
+
+// Student App CI-CT-CD
+pipelineJob('studentapp-project/Complete-CI-CD-Pipeline') {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'configVersion'(2)
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'('https://github.com/cheerlavamsi/jenkins.git')
+           
+          }
+        }
+
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/master')
+          }
+        }
+      }
+      'scriptPath'('jobs/student-proj/proj-ui-complete.jenkinsfile')
+      'lightweight'(true)
+    }
+  }
+}
