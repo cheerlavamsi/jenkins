beae404641c54083e6cf280e9875561e9e3cb353
cheerlavamsi <chvamsi.devops@gmail.com>
2019-12-31 08:22:50 +0530

    add

--- seed-jobs/jobs.groovy
+++ seed-jobs/jobs.groovy
@@ -76,3 +76,29 @@ pipelineJob('studentapp-project/APP-MANUAL-RELEASE') {
   }
 }
 
+//Student Project PROD Infra
+pipelineJob('studentapp-project/PROD-INFRA') {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'configVersion'(2)
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'('https://github.com/cheerlavamsi/jenkins.git')
+            
+          }
+        }
+
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/master')
+          }
+        }
+      }
+      'scriptPath'('jobs/student-proj/proj-prod-infra.jenkinsfile')
+      'lightweight'(true)
+    }
+  }
+}
+
+
